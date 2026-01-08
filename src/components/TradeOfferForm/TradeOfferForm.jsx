e583719cb2430a0f6625d17d3d8066d5ce890ba1
DairiNooh <dairinooh@gmail.com>
2026-01-08 18:02:10 +0300

    Finalized css

--- src/components/TradeOfferForm/TradeOfferForm.jsx
+++ src/components/TradeOfferForm/TradeOfferForm.jsx
@@ -5,6 +5,9 @@ import * as pokemonService from '../../services/pokemonService'
 import { useNavigate } from 'react-router'
 import { useParams } from 'react-router'
 
+import styles from './TradeOfferForm.module.css';
+
+
 function TradeOfferForm({user, updateTradeOfferList, tradeOfferToUpdate, updateOneTradeOffer}) {
 
     const [selectedPokemon, setSelectedPokemon] = useState(null)
@@ -104,70 +107,62 @@ function TradeOfferForm({user, updateTradeOfferList, tradeOfferToUpdate, updateO
     }
 
   return (
-    <div>
-        <h1>Trade Cards</h1>
+    <div className={styles.pageWrapper}>
+  <div className={styles.cardContainer}>
+    <h1 className={styles.title}>Trade Cards</h1>
 
-        <div>
-            {selectedPokemon ? 
-            <div>
-            <div>
-                {selectedPokemon.image && ( <img src={selectedPokemon.image} alt="Preview" style={{ width: "200px", marginTop: "10px" }} /> )}
-                <p>{selectedPokemon.name}</p>
-                <p>{selectedPokemon.title}</p>
-                <p>{selectedPokemon.level}</p>
-                {selectedPokemon.shiny ? <p>Shiny: Yes</p> : <p>Shiny: No</p>}
-            </div>
-            <div>
-                <p>Sender: {user.username}</p>
-            </div>
-            </div>
-             :
-             <h3>Select a Pokemon to trade</h3>
-             }
-        </div>
-        <div>
-            <div>
-                {pokemonToTrade.image && ( <img src={pokemonToTrade.image} alt="Preview" style={{ width: "200px", marginTop: "10px" }} /> )}
-                <p>{pokemonToTrade.name}</p>
-                <p>{pokemonToTrade.title}</p>
-                <p>{pokemonToTrade.level}</p>
-                {pokemonToTrade.shiny ? <p>Shiny: Yes</p> : <p>Shiny: No</p>}
-            </div>
-            <div>
-                Receiver: {pokemonToTrade?.owner?.username}
-            </div>
-        </div>
+    <div className={styles.pokemonRow}>
+      <div className={styles.pokemonBox}>
+        {selectedPokemon ? (
+          <>
+            {selectedPokemon.image && <img src={selectedPokemon.image} alt="Preview" className={styles.pokemonImage} />}
+            <p>{selectedPokemon.name}</p>
+            <p>{selectedPokemon.title}</p>
+            <p>Level: {selectedPokemon.level}</p>
+            <p>Shiny: {selectedPokemon.shiny ? "Yes" : "No"}</p>
+            <p>Sender: {user.username}</p>
+          </>
+        ) : (
+          <h3>Select a Pokemon to trade</h3>
+        )}
+      </div>
 
-        <button onClick={handleSubmit} >Trade</button>
-
-        <h1>My Cards</h1>
-    {myPokemon.length === 0 ?
-     (<p>No Pokemon cards found</p>
-
-     ) : (
-       
-        myPokemon.map((pokemon)=> (
-        
-        <div key={pokemon._id}>
-        
-        {pokemon.image && ( <img src={pokemon.image} alt="Preview" style={{ width: "200px", marginTop: "10px" }} /> )}
-
-        <h3>Name: {pokemon.name}</h3>
-
-        <p>Type: {pokemon.type}</p>
-
-        <button onClick={() => {selectedCard(pokemon)}}>Select</button>
-            
-        </div> )
-        
-        ) 
-
-     ) 
-     
-     
-     }
-
+      <div className={styles.pokemonBox}>
+        {pokemonToTrade.image && <img src={pokemonToTrade.image} alt="Preview" className={styles.pokemonImage} />}
+        <p>{pokemonToTrade.name}</p>
+        <p>{pokemonToTrade.title}</p>
+        <p>Level: {pokemonToTrade.level}</p>
+        <p>Shiny: {pokemonToTrade.shiny ? "Yes" : "No"}</p>
+        <p>Receiver: {pokemonToTrade?.owner?.username}</p>
+      </div>
     </div>
+
+    <button className={styles.actionBtn} onClick={handleSubmit}>
+      Trade
+    </button>
+
+    <h1 className={styles.title}>My Cards</h1>
+    <div className={styles.myPokemonRow}>
+      {myPokemon.length === 0 ? (
+        <p>No Pokemon cards found</p>
+      ) : (
+        myPokemon.map((pokemon) => (
+          <div key={pokemon._id} className={styles.pokemonBox}>
+            {pokemon.image && <img src={pokemon.image} alt="Preview" className={styles.pokemonImage} />}
+            <h3>{pokemon.name}</h3>
+            <p>Type: {pokemon.type}</p>
+            <button className={styles.actionBtn} onClick={() => selectedCard(pokemon)}>
+              Select
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  </div>
+</div>
+
+
+
   )
 }
 
